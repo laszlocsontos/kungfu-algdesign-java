bc559bd143d31b6a1f81ac9f501b35ad2d19009b
Laszlo Csontos <laszlo.csontos@liferay.com>
2014-11-18 13:45:08 +0100

    Finish graph implementation

--- src/main/java/kungfu/algdesign/graph/Graph.java
+++ src/main/java/kungfu/algdesign/graph/Graph.java
@@ -18,22 +18,57 @@
 
 package kungfu.algdesign.graph;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
 
 /**
  * @author lcsontos
  */
 public class Graph {
 
-  public Graph() {
-    vertices = new HashMap<>();
+  public static Graph load(InputStream inputStream) throws InvalidGraphException {
+    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+    Graph graph = new Graph();
+
+    String line = null;
+
+    int lineNumber = 0;
+
+    try {
+      while ((line = bufferedReader.readLine()) != null) {
+        lineNumber++;
+
+        String[] vertices = line.split("\\W");
+
+        if (vertices.length != 2) {
+          throw new InvalidGraphException("Line \"" + lineNumber + "\" is invalid.");
+        }
+
+        graph.addEdge(vertices[1], vertices[0]);
+      }
+    } catch (IOException ioe) {
+      throw new InvalidGraphException("Error reading graph", ioe);
+    }
+
+    return graph;
   }
 
-  public Edge addEdge(String headName, String tailName) {
-    Vertex head = addVertex(headName);
-    Vertex tail = addVertex(tailName);
+  public Graph() {
+    vertices = new LinkedHashMap<>();
+  }
 
+  public Edge addEdge(Vertex head, Vertex tail) {
     Edge edge = new Edge(head, tail);
 
     head.addIncomingEdge(edge);
@@ -42,13 +77,26 @@ public class Graph {
     return edge;
   }
 
+  public Edge addEdge(String headName, String tailName) {
+    Vertex head = addVertexIfNotExists(headName);
+    Vertex tail = addVertexIfNotExists(tailName);
+
+    return addEdge(head, tail);
+  }
+
   public Vertex addVertex(String name) {
+    Vertex vertex = new Vertex(name);
+
+    vertices.put(name, vertex);
+
+    return vertex;
+  }
+
+  public Vertex addVertexIfNotExists(String name) {
     Vertex vertex = getVertex(name);
 
     if (vertex == null) {
-      vertex = new Vertex(name);
-
-      vertices.put(name, vertex);
+      vertex = addVertex(name);
     }
 
     return vertex;
@@ -67,9 +115,25 @@ public class Graph {
   public Graph reverse() {
     Graph reverseGraph = new Graph();
 
-    // TODO reverse
+    for (Vertex vertex : vertices.values()) {
+      Collection<Edge> edges = vertex.getOutgoingEdges();
+
+      for (Edge edge : edges) {
+        String headName = edge.getHead().getName();
+        String tailName = edge.getTail().getName();
+
+        reverseGraph.addEdge(tailName, headName);
+      }
+    }
 
     return reverseGraph;
+  }
+
+  @Override
+  public String toString() {
+    ToStringHelper helper = MoreObjects.toStringHelper(this).add("vertexCount", vertices.size());
+
+    return helper.toString();
   }
 
   private final Map<String, Vertex> vertices;
---
+++ src/test/java/kungfu/algdesign/graph/GraphTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 - present, Laszlo Csontos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package kungfu.algdesign.graph;
+
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
+import org.junit.Test;
+
+import kungfu.algdesign.util.Utils;
+
+/**
+ * @author lcsontos
+ */
+public class GraphTest {
+
+  @Test
+  public void testLoad() throws Exception {
+    InputStream inputStream = new GZIPInputStream(
+      Utils.getInputStreamForResource(GRAPH_INPUT_NAME));
+
+    Graph.load(inputStream);
+  }
+
+  private static final String GRAPH_INPUT_NAME = "SCC.txt.gz";
+
+}
